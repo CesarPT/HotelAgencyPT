675856d4ea0f47714e79804dc4e475b628b1027e
Rodrigo-Filipe-Morais <115590961+Rodrigo-Filipe-Morais@users.noreply.github.com>
2022-11-18 12:07:50 +0000

    melhoramento do controller da reserva

--- HotelAgencyPT/src/main/java/hotel/agencypt/Controller/C_Reserva.java
+++ HotelAgencyPT/src/main/java/hotel/agencypt/Controller/C_Reserva.java
@@ -1,30 +1,22 @@
 package hotel.agencypt.Controller;
 
-import javafx.beans.InvalidationListener;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.*;
 
 public class C_Reserva implements Initializable {
 
-
     @FXML
-    protected Button creatReserva ;
-
-    @FXML
-    public ComboBox quarto ;
+    public ComboBox cboxquarto ;
     @FXML
     ObservableList<String> listTquarto = FXCollections.observableArrayList("Singular","Duplo","Familiar");
 
@@ -34,23 +26,41 @@ public class C_Reserva implements Initializable {
     private ListView<String> listServesco;
 
     String[] servico ={"servico 1","servico 2","servico 3","servico 4"};
-    String servicoselc;
+    String servicoselct;
+    String servicoselce;
+
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        quarto.setItems(listTquarto);
+        cboxquarto.setItems(listTquarto);
 
         listServtodos.getItems().addAll(servico);
         listServtodos.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                servicoselc=listServtodos.getSelectionModel().getSelectedItem();
-                //teste.setText(reservasel);
+                servicoselct=listServtodos.getSelectionModel().getSelectedItem();
+            }
+        });
 
+        listServesco.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                servicoselce=listServesco.getSelectionModel().getSelectedItem();
             }
         });
 
     }
+
+    public void onAdicionar(){
+        listServesco.getItems().add(servicoselct);
+        listServtodos.getItems().remove(servicoselct);
+    }
+
+    public void onRemover(){
+        listServtodos.getItems().add(servicoselce);
+        listServesco.getItems().remove(servicoselce);
+    }
+
 
 }
