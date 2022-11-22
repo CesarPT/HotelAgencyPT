684c51070776eff92168437581f102e6dd8017df
Rodrigo-Filipe-Morais <115590961+Rodrigo-Filipe-Morais@users.noreply.github.com>
2022-11-22 18:26:45 +0000

    implementaçao do XML so imprime na consola

--- HotelAgencyPT/src/main/java/hotel/agencypt/HelloApplication.java
+++ HotelAgencyPT/src/main/java/hotel/agencypt/HelloApplication.java
@@ -6,23 +6,33 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
+
+import static hotel.agencypt.Controller.XMLpath.Lexml;
 
 public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
-<<<<<<< HEAD
-
-        Parent root = FXMLLoader.load(getClass().getResource("Cliente.fxml"));
-=======
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
->>>>>>> d0b5c53244b125d85b3de6ece4c66aa30dc5bd7d
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(new Scene(root, 520, 400));
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        //metodo para ler o xml
+       // Lexml();
         launch();
+
     }
+
 }
