298ef20c381d778145484bec5c07a5f52b11a3d1
Rodrigo-Filipe-Morais <115590961+Rodrigo-Filipe-Morais@users.noreply.github.com>
2022-11-27 15:25:47 +0000

    quarto dao

--- HotelAgencyPT/src/main/java/Classes/DAO/QuartoDAO.java
+++ HotelAgencyPT/src/main/java/Classes/DAO/QuartoDAO.java
@@ -1,8 +1,15 @@
 package Classes.DAO;
 
+import Classes.Reserva;
 import DataBase.ConnectionDB;
+import hotel.agencypt.Controller.Controller;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuartoDAO {
     private static Connection con;
@@ -15,5 +22,31 @@ public class QuartoDAO {
     }
 
 
+    public static List<Reserva> findQuartoIndividual() {
+        String sql = " SELECT LEFT(descricao, charindex(' ', descricao) - 1) from Quarto where ";
 
+
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        List<Reserva> listreserva = new ArrayList<>();
+
+        try {
+            con = ConnectionDB.establishConnection();
+            stmt = con.prepareStatement(sql);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Reserva reserva = new Reserva();
+                reserva.setIdreserva(rs.getInt("idQuarto"));
+                listreserva.add(reserva);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("[ERRO]: findReserva " + e.getMessage());
+        } finally {
+            ConnectionDB.closeConnection(con, stmt, rs);
+        }
+        return listreserva;
+    }
 }
