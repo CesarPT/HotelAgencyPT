03948f4df571fc8b89fba79397b537525d6052d3
César Guimarães <1210522@isep.ipp.pt>
2022-12-10 17:50:52 +0000

    Concluída tarefa: GH - Ver reclamações/sugestões

--- HotelAgencyPT/src/main/java/hotel/agencypt/Controller/GH_GerirFeedback.java
+++ HotelAgencyPT/src/main/java/hotel/agencypt/Controller/GH_GerirFeedback.java
@@ -24,7 +24,7 @@ import java.util.ResourceBundle;
  */
 public class GH_GerirFeedback implements Initializable {
     private Connection con = ConnectionDB.establishConnection();
-
+    private Connection con2 = ConnectionDB.establishConnection();
     @FXML
     private TableView<Feedback> tableReclamacao;
     @FXML
@@ -39,7 +39,7 @@ public class GH_GerirFeedback implements Initializable {
     private TableColumn<Feedback, String> columnDescricao2;
 
     ObservableList<Feedback> obsReclamacao = FXCollections.observableArrayList();
-
+    ObservableList<Feedback> obsSugestao = FXCollections.observableArrayList();
     /**
      * Volta atrás para a View GestorHotel.fxml
      *
@@ -57,30 +57,55 @@ public class GH_GerirFeedback implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         String sql = "SELECT idcliente, descricao\n" +
-                "FROM Feedback\n" +
-                "WHERE tipofeedback='R'";
+                        "FROM Feedback\n" +
+                        "WHERE tipofeedback='R'";
 
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
+                PreparedStatement stmt = null;
+                ResultSet rs = null;
 
-        //Limpar tudo e Adicionar todos os cartões
-        try {
-            stmt = con.prepareStatement(sql);
-            rs = stmt.executeQuery();
+                //Limpar tudo e Adicionar todos os cartões
+                try {
+                    stmt = con.prepareStatement(sql);
+                    rs = stmt.executeQuery();
 
-            while (rs.next()) {
-                Integer idcliente = rs.getInt("idcliente");
-                String descricao = rs.getString("descricao");
+                    while (rs.next()) {
+                        Integer idcliente = rs.getInt("idcliente");
+                        String descricao = rs.getString("descricao");
                 obsReclamacao.add(new Feedback(idcliente, descricao));
             }
             //Colocar os valores nas colunas da TableView
-            columnID.setCellValueFactory(new PropertyValueFactory<>("idcliente"));
+            columnID.setCellValueFactory(new PropertyValueFactory<>("id_cliente"));
             columnDescricao.setCellValueFactory(new PropertyValueFactory<>("descricao"));
             tableReclamacao.setItems(obsReclamacao);
         } catch (SQLException e) {
             System.err.println("[ERRO]: initialize " + e.getMessage());
+        }
+
+        String sql2 = "SELECT idcliente, descricao\n" +
+                "FROM Feedback\n" +
+                "WHERE tipofeedback='S'";
+
+        PreparedStatement stmt2 = null;
+        ResultSet rs2 = null;
+
+        //Limpar tudo e Adicionar todos os cartões
+        try {
+            stmt2 = con2.prepareStatement(sql2);
+            rs2 = stmt2.executeQuery();
+
+            while (rs2.next()) {
+                Integer idcliente = rs2.getInt("idcliente");
+                String descricao = rs2.getString("descricao");
+                obsSugestao.add(new Feedback(idcliente, descricao));
+            }
+            //Colocar os valores nas colunas da TableView
+            columnID2.setCellValueFactory(new PropertyValueFactory<>("id_cliente"));
+            columnDescricao2.setCellValueFactory(new PropertyValueFactory<>("descricao"));
+            tableSugestao.setItems(obsSugestao);
+        } catch (SQLException e) {
+            System.err.println("[ERRO]: initialize " + e.getMessage());
         } finally {
-            ConnectionDB.closeConnection(con, stmt, rs);
+            ConnectionDB.closeConnection(con2, stmt2, rs2);
         }
     }
 }
