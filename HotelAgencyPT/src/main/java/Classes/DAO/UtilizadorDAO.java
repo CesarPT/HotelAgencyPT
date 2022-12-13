2001f729eada492d5a40615ee665e8e55e2893b8
Tiago Ferraz <1210537@isep.ipp.pt>
2022-12-13 22:54:18 +0000

    Merging

---
+++ HotelAgencyPT/src/main/java/Classes/DAO/UtilizadorDAO.java
@@ -0,0 +1,63 @@
+package Classes.DAO;
+
+import Classes.Cliente;
+import Classes.Utilizador;
+import DataBase.ConnectionDB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UtilizadorDAO {
+
+    private static Connection con = ConnectionDB.establishConnection();
+
+
+    public static List<Utilizador> findClientepReserva() {
+        String sql = "Select Utilizador.nomeuser from Utilizador INNER JOIN Cliente on Cliente.iduser=Utilizador.iduser";
+        List<Utilizador> listUser = new ArrayList<>();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            stmt = con.prepareStatement(sql);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Utilizador utilizador = new Utilizador();
+                utilizador.setNomeUtilizador(rs.getString("nomeuser"));
+                listUser.add(utilizador);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("[ERRO]: findClientepReserva " + e.getMessage());
+        }
+        return listUser;
+    }
+
+
+    public static List<Utilizador> findClientepReservaCnome(String nomeclient) {
+        String sql = "Select Utilizador.nomeuser from Utilizador INNER JOIN Cliente " +
+                "on Cliente.iduser=Utilizador.iduser and Utilizador.nomeuser= '" + nomeclient + "'";
+
+
+        List<Utilizador> listUser = new ArrayList<>();
+
+        try {
+            PreparedStatement stmt = con.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Utilizador utilizador = new Utilizador();
+                utilizador.setNomeUtilizador(rs.getString("nomeuser"));
+                listUser.add(utilizador);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("[ERRO]: findClientepReserva123 " + e.getMessage());
+        }
+        return listUser;
+    }
+}
