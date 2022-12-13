e8611adf1f9c1d7a78b51b5973c4d0a13443acbc
César Guimarães <1210522@isep.ipp.pt>
2022-12-13 10:07:33 +0000

    Concluída tarefa: Ver produtos por quarto

---
+++ HotelAgencyPT/src/main/java/Classes/DAO/QuartoStockDAO.java
@@ -0,0 +1,50 @@
+package Classes.DAO;
+
+import Classes.QuartoStock;
+import DataBase.ConnectionDB;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuartoStockDAO {
+    private static Connection con = ConnectionDB.establishConnection();
+
+    /**
+     * Método para pesquisar o idquarto, descrição do produto e quantidade
+     * @return lista
+     */
+    public List<QuartoStock> findQuartoStock() {
+        String sql = "SELECT QuartoStock.idquarto, Stock.product_description, QuartoStock.quantidade\n" +
+                "FROM QuartoStock\n" +
+                "INNER JOIN Stock\n" +
+                "ON Stock.product_identifier = QuartoStock.idstock\n" +
+                "ORDER BY QuartoStock.idquarto";
+
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        List<QuartoStock> listQuartoStock = new ArrayList<>();
+
+        try {
+            stmt = con.prepareStatement(sql);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                QuartoStock quarto = new QuartoStock();
+                quarto.setIdquarto(rs.getInt("idquarto"));
+                quarto.setProduct_description(rs.getString("product_description"));
+                quarto.setQuantidade(rs.getFloat("quantidade"));
+                listQuartoStock.add(quarto);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("[ERRO]: findQuartoStock " + e.getMessage());
+        } finally {
+            ConnectionDB.closeConnection(con, stmt, rs);
+        }
+        return listQuartoStock;
+    }
+}
--- HotelAgencyPT/src/main/java/hotel/agencypt/Controller/StockQuartos.java
+++ HotelAgencyPT/src/main/java/hotel/agencypt/Controller/StockQuartos.java
@@ -1,9 +1,66 @@
 package hotel.agencypt.Controller;
 
+import Classes.DAO.QuartoStockDAO;
+import Classes.Quarto;
+import Classes.QuartoStock;
+import Classes.RegEntrada;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
+import javafx.util.Callback;
 
-public class StockQuartos {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class StockQuartos implements Initializable {
+    QuartoStockDAO qsDAO = new QuartoStockDAO();
+    List<QuartoStock> arrayQuartoStock = new ArrayList<>();
+    ObservableList<QuartoStock> obsQuartoStock = FXCollections.observableArrayList();
+
+    @FXML
+    private TableView<QuartoStock> tableStockQuarto;
+    @FXML
+    private TableColumn<QuartoStock, Integer> columnQuarto;
+    @FXML
+    private TableColumn<QuartoStock, String> columnDescricao;
+    @FXML
+    private TableColumn<QuartoStock, Float> columnQuantidade;
+
+    @FXML
+    private TableColumn<QuartoStock, String> columnEstado;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+                arrayQuartoStock = qsDAO.findQuartoStock();
+                for (QuartoStock qs : arrayQuartoStock) {
+                    Integer idquarto = qs.getIdquarto();
+                    String pd = qs.getProduct_description();
+                    Float qtd = qs.getQuantidade();
+                    String estado;
+                    if (qtd <= 3){
+                        qs.setEstado("Critico!");
+                    } else if (qtd <= 9) {
+                        qs.setEstado("Pouco stock");
+                    } else {
+                        qs.setEstado("Stock suficiente");
+                    }
+                    estado = qs.getEstado();
+                    obsQuartoStock.add(new QuartoStock(idquarto, pd, qtd, estado));
+            }
+        columnQuarto.setCellValueFactory(new PropertyValueFactory<>("idquarto"));
+        columnDescricao.setCellValueFactory(new PropertyValueFactory<>("product_description"));
+        columnQuantidade.setCellValueFactory(new PropertyValueFactory<>("quantidade"));
+        columnEstado.setCellValueFactory(new PropertyValueFactory<>("estado"));
+        tableStockQuarto.setItems(obsQuartoStock);
+    }
+
 
     /**
      * Volta atrás
@@ -21,4 +78,5 @@ public class StockQuartos {
             System.out.println("Erro ao voltar atrás.");
         }
     }
+
 }
